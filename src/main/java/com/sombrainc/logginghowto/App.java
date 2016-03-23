d34e9c9840e23ce7c0765f69f5201d7433232428
ostryzhniukn <nazar.ostryzhniuk@gmail.com>
2016-03-23 13:40:56 +0200

    Done

---
+++ src/main/java/com/sombrainc/logginghowto/App.java
@@ -0,0 +1,17 @@
+package com.sombrainc.logginghowto;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		LOGGER.trace("Message");
+		LOGGER.debug("Message");
+		LOGGER.info("Message");
+		LOGGER.warn("Message");
+		LOGGER.error("Message");
+	}
+}
